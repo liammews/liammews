3fbd97ea901453f858d022246a33b0b40851dfb6
liammews <liam.mews@outlook.com>
2021-02-18 00:28:50 +0000

    Fixing music collection

--- web/src/components/music/musiccard.js
+++ web/src/components/music/musiccard.js
@@ -3,13 +3,16 @@ import React from "react";
 const musicCard = ({ article }) => {
   return (
         <a className="no-underline" href={`${article.node.link}`}>
-        <div className="flex flex-col justify-center">
-        <div><img src={`${article.node.coverImage.asset.fluid.src}`} alt={`${article.node.title} album cover`} className="lg:h-48 lg:w-48 rounded-md mb-2 shadow-lg"></img></div>
+        <div className="flex flex-row md:flex-col flex-nowrap md:flex-wrap md:justify-center my-6 md:my-8 md:w-48">
+
+        <div className="flex-shrink-0"><img src={`${article.node.coverImage.asset.fluid.src}`} alt={`${article.node.title} album cover`} className="h-48 w-48 rounded-md mb-2 shadow-lg mr-4 md:mr-0"></img></div>
+
+        <div className="self-end md:self-start pb-2 md:pb-0">
         <div><p className="text-blue-500 dark:text-blue-300 text-sm font-medium no-underline mb-0">{article.node.title}</p></div>
         <div><p className="text-black dark:text-white text-xs no-underline mb-0">{article.node.artist}</p></div>
-        <div className="flex flex-row">
-        <p className="text-gray-400  dark:text-gray-300  text-xs mb-0">{article.node.category} - {article.node.date}</p>
-          </div>
+        <div><p className="text-gray-400  dark:text-gray-300  text-xs mb-0">{article.node.category} - {article.node.date}</p></div>
+        </div>
+
         </div>
         </a>
   );
